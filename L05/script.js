c2f6ab87a8a8fa95c2f7838971caee911dd28535
MagdalenaNitz <magdalenanitz@gmx.de>
2023-12-03 13:14:05 +0100

    L05

---
+++ L05/script.js
@@ -0,0 +1,25 @@
+let myVariable = true;
+
+console.log (myVariable)
+
+let myMovie = prompt ("Welchen Film möchtest du schauen? 'The Marvels' (a) oder 'Die Tribute von Panem' (b)?");
+
+console.log (myMovie)
+
+if (myMovie == "a" || myMovie == "b" ) {
+    let snackChoice = prompt ("Gute Wahl. fehlt nur noch der passende Snack. Süßes oder salziges Popcorn?");
+
+    if (snackChoice == "süßes") {
+        let snackAnswer = prompt ("Du hast süßes Popcorn gewählt. Das hätte ich auch gemacht. Genieße den Film!");
+    }
+    else if (snackChoice == "salziges") {
+        let snackAnswer = prompt ("Wirklich?! Du hast salziges Popcorn gewählt. Das schmeckt doch nicht. Genieße trotzdem den Film!");
+    }
+    else {
+        prompt (" Keine korrekte Eingabe. Wähle 'süßes' ODER 'salziges'")
+    }
+    }
+
+else {
+    prompt ("Keine korrekte Eingabe. Wähle a oder b.")
+    }
