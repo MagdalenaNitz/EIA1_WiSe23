cb325e5bd758124187ae8c822c26572ad53d8df9
MagdalenaNitz <magdalenanitz@gmx.de>
2023-12-03 13:16:14 +0100

    Update script.js

--- L05/script.js
+++ L05/script.js
@@ -7,7 +7,7 @@ let myMovie = prompt ("Welchen Film möchtest du schauen? 'The Marvels' (a) oder
 console.log (myMovie)
 
 if (myMovie == "a" || myMovie == "b" ) {
-    let snackChoice = prompt ("Gute Wahl. fehlt nur noch der passende Snack. Süßes oder salziges Popcorn?");
+    let snackChoice = prompt ("Gute Wahl. Fehlt nur noch der passende Snack. Süßes oder salziges Popcorn?");
 
     if (snackChoice == "süßes") {
         let snackAnswer = prompt ("Du hast süßes Popcorn gewählt. Das hätte ich auch gemacht. Genieße den Film!");
@@ -16,7 +16,7 @@ if (myMovie == "a" || myMovie == "b" ) {
         let snackAnswer = prompt ("Wirklich?! Du hast salziges Popcorn gewählt. Das schmeckt doch nicht. Genieße trotzdem den Film!");
     }
     else {
-        prompt (" Keine korrekte Eingabe. Wähle 'süßes' ODER 'salziges'")
+        prompt (" Keine korrekte Eingabe. Wähle 'süßes' oder 'salziges'")
     }
     }
 
